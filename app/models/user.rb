e0a1c9281c4fc5bcd031c03f6d8e3480af971a35
EC2 Default User <ec2-user@ip-172-31-1-145.us-east-2.compute.internal>
2018-04-01 03:43:08 +0000

    Fixed relationship of users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
     has_many :drafts, dependent: :destroy
     has_many :relationships, foreign_key: "followed_user_id", dependent: :destroy
-    has_many :followers, through: relationships, source: follower
+    has_many :followers, through: relation
     
+    # def following?(other_user)
+    # relationships.find_by(followed_user_id: other_user.id)
+    # end
+
+    # def follow!(other_user)
+    # relationships.create!(followed_id: other_user.id)
+    # end
     
 end
